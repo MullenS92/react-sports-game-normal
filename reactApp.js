2aece7a726305c5a4e10a7560857ff7195813455
Shanita Mullen <shanitam92@gmail.com>
2020-02-04 22:53:32 -0500

    medium mode

--- reactApp.js
+++ reactApp.js
@@ -5,62 +5,128 @@ class Team extends React.Component {
     this.state = {
       shots: 0,
       score: 0
-    };
+    
+  }
+    this.shotSound = new Audio('./audio/shoot.mp3')
+this.scoreSound = new Audio('/audio/score.mp3')
   }
 
-  shotHandler = () => {
-    let score = this.state.score;
+shotHandler = () => {
+  let score = this.state.score;
 
-    if (Math.random() > 0.5) {
-      score += 1;
-    }
+    this.shotSound.play()
+  
+    
 
-    this.setState((state, props) => ({
-      shots: state.shots + 1,
-      score
-    }));
-  };
+  if (Math.random() > 0.5) {
+    score += 1;
 
-  render() {
-    return (
-      <div className="Team">
-        <h2>{this.props.name}</h2>
-        <div className="identity">
-          <img src={this.props.logo} alt={this.props.name} />
-        </div>
-        <div>
-          <strong>Shots:</strong> {this.state.shots}
-        </div>
-        <div>
-          <strong>Score:</strong> {this.state.score}
-        </div>
-        <button onClick={this.shotHandler}>Shoot!</button>{" "}
+    setTimeout(() => {
+    this.scoreSound.play()
+  }, 100)
+
+  }
+
+  this.setState((state, props) => ({
+    shots: state.shots + 1,
+    score
+  }));
+};
+
+render() {
+  let shotPercentageDiv
+
+  if (this.state.shots) {
+  const shotPercentage =  Math.round((this.state.score / this.state.shots) * 100)
+  shotPercentageDiv = (
+    <div>
+    <strong>Shooting %:</strong> {shotPercentage}
+  </div>
+)
+  }
+  return (
+    <div className="Team">
+      <h2>{this.props.name}</h2>
+      <div className="identity">
+        <img src={this.props.logo} alt={this.props.name} />
       </div>
-    );
-  }
+      <div>
+        <strong>Shots:</strong> {this.state.shots}
+      </div>
+      <div>
+        <strong>Score:</strong> {this.state.score}
+      </div>
+    
+
+      {shotPercentageDiv}
+
+      <button onClick={this.shotHandler}>Shoot!</button>{" "}
+    </div>
+  );
+}
 }
 
-// Deafault App component that all other compents are rendered through
-function App(props) {
+function Game(props){
   return (
-    <div className="App">
+    <div className="Game">
+            <h1>Welcome to {props.venue}</h1>
       <div className="stats">
         <Team
-          name="Russiaville Raccoons"
-          logo="https://cdn.mos.cms.futurecdn.net/YYH9o4wmSXJfvbzRTq5BTY-320-80.jpg"
+          name={props.visitingTeam.name}
+          logo={props.visitingTeam.logoSrc}
         />
 
         <div className="versus">
           <h1>VS</h1>
         </div>
 
-        <Team
-          name="Sheridan Squirrels"
-          logo="https://media.npr.org/assets/img/2017/04/25/istock-115796521-fcf434f36d3d0865301cdcb9c996cfd80578ca99-s800-c85.jpg"
+      <Team
+        name={props.homeTeam.name}
+        logo={props.homeTeam.logoSrc}
+          
         />
       </div>
     </div>
-  );
+  )
+}
+
+// Deafault App component that all other compents are rendered through
+function App(props) {
+  const Cubbies = {
+    name: 'Chicago Cubbies',
+    logoSrc: './images/bears.png'
+  }
+  const Ponies = {
+    name: 'Pheonix Ponies',
+    logoSrc: './images/broncos.png'
+  }
+
+  const Bunnies = {
+    name: 'Baltimore Bunnies',
+    logoSrc: './images/bunny.jpg'
+  }
+
+  const Wolvesies = {
+    name: 'Washington Wolvesies',
+    logoSrc: './images/wolves.png'
+  }
+
+
+  return (
+    <div className="App">
+    <Game 
+      venue="Cubbies Stadium"
+      homeTeam={Cubbies} 
+      visitingTeam={Ponies}
+      />
+    <Game
+     venue="Bunnies Stadium"
+     homeTeam={Bunnies}
+     visitingTeam={Wolvesies}
+      />
+
+  </div> 
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
